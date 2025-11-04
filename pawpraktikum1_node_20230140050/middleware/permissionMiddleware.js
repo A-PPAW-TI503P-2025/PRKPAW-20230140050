adde4691dfc541a311519e4cf3d156cd9bf6af4f
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-11-04 21:01:21 +0700

    PRAKTIKUM PERTEMUAN 5

--- pawpraktikum1_node_20230140050/middleware/permissionMiddleware.js
+++ pawpraktikum1_node_20230140050/middleware/permissionMiddleware.js
@@ -1,16 +1,15 @@
 exports.addUserData = (req, res, next) => {
-  console.log("Middleware: Menambahkan data user dummy...");
+  console.log("Middleware: Menambahkan data user ke tiruan (dummy)..");
   req.user = {
     id: 123,
-    nama: "User Karyawan",
+    nama: "Refky Muhammad Syahrin",
     role: "admin",
   };
   next();
 };
-
 exports.isAdmin = (req, res, next) => {
   if (req.user && req.user.role === "admin") {
-    console.log("Middleware: Izin admin diberikan.");
+    console.log("Middleware: apakah user adalah admin..");
     next();
   } else {
     console.log("Middleware: Gagal! Pengguna bukan admin.");
