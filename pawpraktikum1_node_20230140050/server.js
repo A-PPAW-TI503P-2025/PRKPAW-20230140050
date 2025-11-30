cacf51a6deed62cc939a4bb334e731bfe99b69b8
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-11-30 19:10:36 +0700

    inisialisasi server Express dengan konfigurasi route dan middleware

--- pawpraktikum1_node_20230140050/server.js
+++ pawpraktikum1_node_20230140050/server.js
@@ -1,32 +1,37 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const morgan = require("morgan");
 
 // Impor router
 const presensiRoutes = require("./routes/presensi");
 const reportRoutes = require("./routes/reports");
-
-// Memberi tahu aplikasi Express untuk menggunakan route baru yg telah dibuat.
-const authRoutes = require("./routes/auth"); // Impor rute auth
+const authRoutes = require("./routes/auth");
+const ruteBuku = require("./routes/books");
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+
 app.use((req, res, next) => {
   console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
   next();
 });
+
 app.get("/", (req, res) => {
   res.send("Home Page for API");
 });
-const ruteBuku = require("./routes/books");
-app.use("/api/books", ruteBuku);
-app.use("/api/presensi", presensiRoutes);
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/attendance", presensiRoutes);
 app.use("/api/reports", reportRoutes);
-app.use("/api/auth", authRoutes); // Daftarkan rute /api/auth
+app.use("/api/books", ruteBuku);
+
 app.listen(PORT, () => {
   console.log(`Express server running at http://localhost:${PORT}/`);
 });
